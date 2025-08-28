e8f4bac7d36624fcd0cbe2e01a9f099910ce0e2e
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-08-28 23:47:38 +0600

    create a CustomDock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import ContactMe from "@/components/ContactMe";
+import { CustomDock } from "@/components/CustomDock";
 import Education from "@/components/Education";
 import IntroSection from "@/components/IntroSection";
 import Projects from "@/components/Projects";
@@ -15,6 +16,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col md:gap-12 gap-8 md:p-24 p-10 relative overflow-hidden lg:w-8/12 sm:w-full mx-auto">
       <div  className="fixed inset-0 pointer-events-none light-mode-fade-bottom dark:fade-bottom z-10 bg-white/10 dark:bg-black/10"  />
+      
+      <CustomDock />
+      
       <IntroSection />
       <Skills />
       <WorkExperience />
